12f8fb087a6d856f3232861145081bd82276d4dd
Vishwh <Vishwajeet2349@gmail.com>
2026-09-19 16:54:14 +0530

    Configure GitHub Pages

--- currency-Convertor/vite.config.js
+++ currency-Convertor/vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/React/currency-Convertor/',
   plugins: [react(), tailwindcss()],
 })
